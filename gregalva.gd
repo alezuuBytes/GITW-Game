af544f2f2284984f4d50ba6b61b49d7791726e47
alezuuBytes <alexmine9999@gmail.com>
2026-04-25 18:22:52 -0300

    alezuu commit test

--- gregalva.gd
+++ gregalva.gd
@@ -1,14 +1,9 @@
 extends CharacterBody2D
 
-var text := 12
+var input : Vector2
+var speed := 1000
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print(text)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		#text = "hola"
-		print(text)
+func _physics_process(delta: float) -> void:
+	input = Input.get_vector("left","right","up","down")
+	velocity = velocity.lerp(input*speed,delta * 10)
+	move_and_slide()
