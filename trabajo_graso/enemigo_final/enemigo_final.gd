81c257e9ec81ebb3fcbc7aed107c8aa60defce6e
graso <vivalagrasa6996@gmail.com>
2026-04-26 17:38:54 -0300

    carpeta de graso
    toy intentando subir algo a github, porfavor q no se rompa nada

---
+++ trabajo_graso/enemigo_final/enemigo_final.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+class_name EnemigoFinal
+
+@export var speed := 200
+@export var accel := 9.5
+@export var player : CharacterBody2D = null
+
+func _physics_process(delta: float) -> void:
+	
+	if player != null:
+		velocity = velocity.lerp(global_position.direction_to(player.global_position) * speed,delta * accel)
+	move_and_slide()
