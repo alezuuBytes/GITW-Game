442a5f1ff389825983e3c64cfcd3c9ba6974a88c
alezuuBytes <alexmine9999@gmail.com>
2026-05-07 00:49:45 -0300

    lol

--- Escenas/Escena_Principal/escena_principal.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is CharacterBody2D:
-		globales.posicion_personaje = body.global_position
-		globales.velocidad_personaje = body.velocity
-		
-		get_tree().change_scene_to_file("res://Escenas/Escena_Final/EscenaFinal.tscn")
---
+++ scripts/escena_final.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_file("res://Escenas/ganar_juego.tscn")
